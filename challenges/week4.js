b613dac6b9d32b12f875390b5a412d13c1e652b4
Nichole Spencer <nic_spencer@hotmail.co.uk>
2020-04-30 16:49:48 +0100

    attempt 1

--- challenges/week4.js
+++ challenges/week4.js
@@ -1,44 +1,82 @@
+//Q1 - COMPLETE
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let result = nums.filter(num =>
+    (num < 1)
+  )
+  return result;
 }
 
+//Q2 - COMPLETE
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+    let result = names.filter(name =>
+      (name.charAt(0) === char)
+
+    )
+    return result
 }
 
+//Q3 - COMPLETE
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // Your code here
+  let result = words.filter(phrase =>
+    (phrase.charAt(0,1) === 'to')
+
+  )
+  return result
 }
 
+//Q4 - COMPLETE
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let result = nums.filter(num =>
+    (num % 1 === 0)
+  )
+  return result;
 }
 
-function getCities(users) {
-  if (!users) throw new Error("users is required");
-  // Your code here
+//Q5-***INCOMPLETE*** - Returns undefined
+function getCities(users){ 
+  let results= users.map(person =>
+    (person.displayName)
+    )
+   return results;
 }
+  
+ 
+    
+    
 
+//Q6 ***NEARLY COMPLETE - slight rounding error***
 function getSquareRoots(nums) {
-  if (!nums) throw new Error("nums is required");
-  // Your code here
+if (!nums) throw new Error ("nums is required");
+let result = nums.map(x =>  Math.sqrt(x).toFixed(2));
+return result
 }
 
+
+//Q7 - COMPLETE
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  // Your code here
+  str =str.toLowerCase()
+  let results = sentences.filter(sentence=>
+    sentence.toLowerCase().includes(str) 
+  )
+  return results
 }
 
+//Q8- COMPLETE
 function getLongestSides(triangles) {
-  if (!triangles) throw new Error("triangles is required");
-  // Your code here
+
+ let results = triangles.map(individualLength => Math.max.apply(null, individualLength))
+ return results
 }
+
+//Helpful article - https://stackoverflow.com/questions/41961809/find-largest-number-in-array-of-arrays
+//Not sure how to use the apply method fully, removing the 'null' paramenter results in an array of infinity?
 
 module.exports = {
   findSmallNums,
